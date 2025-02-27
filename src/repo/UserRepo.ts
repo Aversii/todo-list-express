d1c6c2a73d9e896cd9de0b63c83f4e0412bfe365
aversii <lucasaversi@gmail.com>
2025-02-27 18:55:56 -0300

    feat: repository for user with prisma created and prisma client set

---
+++ src/repo/UserRepo.ts
@@ -0,0 +1,45 @@
+import { User } from "../model/user/User";
+import prisma from "./PrismaClient/PrismaClient";
+
+export const UserRepo = {
+  async createUser(user: User) {
+    return await prisma.user.create({
+      data: {
+        id: user.GetId(),
+        email: user.GetEmail(),
+        name: user.GetName(),
+        password: user.GetPassword(),
+      },
+    });
+  },
+
+  async getUserByEmail(email: string) {
+    return await prisma.user.findUnique({
+      where: { email },
+    });
+  },
+
+  async getAllUsers() {
+    return await prisma.user.findMany();
+  },
+
+  async updateUser(id: string, data: Partial<User>) {
+    return await prisma.user.update({
+      where: { id },
+      data,
+    });
+  },
+
+  async deleteUser(id: string) {
+    return await prisma.user.delete({
+      where: { id },
+    });
+  },
+
+  async getUserById(id: string) {
+    return await prisma.user.findUnique({
+      where: { id },
+    });
+  },
+
+};
